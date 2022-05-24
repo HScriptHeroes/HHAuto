2340e77473be105358097f983ba868c8cd2e2396
Marxulia <rupersama@gmail.com>
2022-05-24 14:51:52 -0300

    TEst

--- UI/UItexts.js
+++ UI/UItexts.js
@@ -1,4 +1,3 @@
-
 function getLanguageCode() {
     alert("getLanguageCode");
     let HHAuto_Lang = 'en';
@@ -18,4 +17,30 @@ function getLanguageCode() {
         HHAuto_Lang = 'it';
     }
     return HHAuto_Lang;
+}
+
+function getTextForUI(id, type) {
+    alert("getTextForUI");
+    let HHAuto_Lang = getLanguageCode();
+    let defaultLanguageText = null;
+    let defaultLanguageVersion = "0";
+
+    //console.log(id);
+    if (HHAuto_ToolTips['en'] !== undefined && HHAuto_ToolTips['en'][id] !== undefined && HHAuto_ToolTips['en'][id][type] !== undefined) {
+        defaultLanguageText = HHAuto_ToolTips['en'][id][type];
+        defaultLanguageVersion = HHAuto_ToolTips['en'][id].version;
+    }
+
+    if (HHAuto_ToolTips[HHAuto_Lang] !== undefined && HHAuto_ToolTips[HHAuto_Lang][id] !== undefined && HHAuto_ToolTips[HHAuto_Lang][id][type] !== undefined && cmpVersions(HHAuto_ToolTips[HHAuto_Lang][id].version, defaultLanguageVersion) >= 0) {
+        return HHAuto_ToolTips[HHAuto_Lang][id][type];
+    }
+    else {
+        if (defaultLanguageText !== null) {
+            return defaultLanguageText;
+        }
+        else {
+            logHHAuto("not found text for " + HHAuto_Lang + "/" + id + "/" + type);
+            return HHAuto_Lang + "/" + id + "/" + type + " not found.";
+        }
+    }
 }
