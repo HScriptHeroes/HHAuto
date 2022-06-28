6064576cd3e8815b933238009045b28ba96b5a5f
Marxulia <rupersama@gmail.com>
2022-06-28 12:05:51 -0300

    New modules created

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -33,8 +33,14 @@
 // @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Module/HHAuto_Pantheon.js?2
 // @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Module/HHAuto_PlacesPower.js?2
 // @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Module/HHAuto_Pachinko.js?2
+// @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Module/HHAuto_PathOf.js?2
+// @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Module/HHAuto_Season.js?2
 // @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Storage/HHAuto_Storage.js?2
 // @require https://raw.githubusercontent.com/HScriptHeroes/HHAuto/main/Storage/HHAuto_Log.js?2
+
+
+
+
 
 // ==/UserScript==
 
@@ -728,70 +734,7 @@ function moduleDisplayContestsDeletion() {
     });
 }
 
-function moduleOldPathOfAttractionHide() {
-    //https://nutaku.haremheroes.com/path-of-attraction.html"
-    let array = $('#path_of_attraction div.poa.container div.all-objectives .objective.completed');
-    if (array.length == 0) {
-        return
-    }
-    let lengthNeeded = $('.golden-block.locked').length > 0 ? 1 : 2;
-    for (let i = array.length - 1; i >= 0; i--) {
-        if ($(array[i]).find('.picked-reward').length == lengthNeeded) {
-            array[i].style.display = "none";
-        }
-    }
-}
-
-function modulePathOfAttractionHide() {
-    if (getPage() === getHHScriptVars("pagesIDEvent") && window.location.search.includes("tab=path_event") && getStoredValue("HHAuto_Setting_PoAMaskRewards") === "true") {
-        let arrayz;
-        let nbReward;
-        let modified = false;
-        arrayz = $('.nc-poa-reward-pair:not([style*="display:none"]):not([style*="display: none"])');
-        if ($("#nc-poa-tape-blocker").length) {
-            nbReward = 1;
-        }
-        else {
-            nbReward = 2;
-        }
-
-        var obj;
-        if (arrayz.length > 0) {
-            for (var i2 = arrayz.length - 1; i2 >= 0; i2--) {
-                obj = $(arrayz[i2]).find('.nc-poa-reward-container.claimed');
-                if (obj.length >= nbReward) {
-                    //console.log("scroll before : "+document.getElementById('rewards_cont_scroll').scrollLeft);
-                    //console.log("width : "+arrayz[i2].offsetWidth);
-                    $("#events .nc-panel-body .scroll-area")[0].scrollLeft -= arrayz[i2].offsetWidth;
-                    //console.log("scroll after : "+document.getElementById('rewards_cont_scroll').scrollLeft);arrayz[i2].style.display = "none";
-                    arrayz[i2].style.display = "none";
-                    modified = true;
-                }
-            }
-        }
-    }
-}
-
-function getPoVRemainingTime() {
-    const poVTimerRequest = `#pov_tab_container > div.potions-paths-first-row > div.potions-paths-timer.timer[${getHHScriptVars("PoVPoGTimestampAttributeName")}]`;
-
-    if ($(poVTimerRequest).length > 0 && (getSecondsLeft("PoVRemainingTime") === 0 || getStoredValue("HHAuto_Temp_PoVEndDate") === undefined)) {
-        const poVTimer = Number($(poVTimerRequest).attr(getHHScriptVars("PoVPoGTimestampAttributeName")));
-        setTimer("PoVRemainingTime", poVTimer);
-        setStoredValue("HHAuto_Temp_PoVEndDate", Math.ceil(new Date().getTime() / 1000) + poVTimer);
-    }
-}
-
-function getPoGRemainingTime() {
-    const poGTimerRequest = `#pog_tab_container > div.potions-paths-first-row > div.potions-paths-timer.timer[${getHHScriptVars("PoVPoGTimestampAttributeName")}]`;
-
-    if ($(poGTimerRequest).length > 0 && (getSecondsLeft("PoGRemainingTime") === 0 || getStoredValue("HHAuto_Temp_PoGEndDate") === undefined)) {
-        const poGTimer = Number($(poGTimerRequest).attr(getHHScriptVars("PoVPoGTimestampAttributeName")));
-        setTimer("PoGRemainingTime", poGTimer);
-        setStoredValue("HHAuto_Temp_PoGEndDate", Math.ceil(new Date().getTime() / 1000) + poGTimer);
-    }
-}
-
+/*TODO: Remove GM_AddStyle*/
 function displayPoVRemainingTime() {
     const displayTimer = $("#scriptPoVTimer").length === 0;
     if (getTimer("PoVRemainingTime") !== -1) {
@@ -816,7 +759,7 @@ function displayPoVRemainingTime() {
         }
     }
 }
-
+/*TODO: Remove GM_AddStyle*/
 function displayPoGRemainingTime() {
     const displayTimer = $("#scriptPoGTimer").length === 0;
     if (getTimer("PoGRemainingTime") !== -1) {
@@ -842,112 +785,8 @@ function displayPoGRemainingTime() {
     }
 }
 
-function moduleSimPoVMaskReward() {
-    var arrayz;
-    var nbReward;
-    let modified = false;
-    arrayz = $('.potions-paths-tier:not([style*="display:none"]):not([style*="display: none"])');
-    //doesn sure about  " .purchase-pov-pass"-button visibility
-    if ($('#pov_tab_container .potions-paths-second-row .purchase-pass:not([style*="display:none"]):not([style*="display: none"])').length) {
-        nbReward = 1;
-    }
-    else {
-        nbReward = 2;
-    }
-    var obj;
-    if (arrayz.length > 0) {
-        for (var i2 = arrayz.length - 1; i2 >= 0; i2--) {
-            obj = $(arrayz[i2]).find('.claimed-slot:not([style*="display:none"]):not([style*="display: none"])');
-            if (obj.length >= nbReward) {
-                //console.log("width : "+arrayz[i2].offsetWidth);
-                //document.getElementById('rewards_cont_scroll').scrollLeft-=arrayz[i2].offsetWidth;
-                arrayz[i2].style.display = "none";
-                modified = true;
-            }
-        }
-    }
-
-    if (modified) {
-        let divToModify = $('.pov-progress-bar-section');
-        if (divToModify.length > 0) {
-            $('.pov-progress-bar-section')[0].scrollTop = '0';
-        }
-    }
-}
-
-function moduleSimPoGMaskReward() {
-    var arrayz;
-    var nbReward;
-    let modified = false;
-    arrayz = $('.potions-paths-tier:not([style*="display:none"]):not([style*="display: none"])');
-    //doesn sure about  " .purchase-pov-pass"-button visibility
-    if ($('#pog_tab_container .potions-paths-second-row .purchase-pass:not([style*="display:none"]):not([style*="display: none"])').length) {
-        nbReward = 1;
-    }
-    else {
-        nbReward = 2;
-    }
-    var obj;
-    if (arrayz.length > 0) {
-        for (var i2 = arrayz.length - 1; i2 >= 0; i2--) {
-            obj = $(arrayz[i2]).find('.claimed-slot:not([style*="display:none"]):not([style*="display: none"])');
-            if (obj.length >= nbReward) {
-                //console.log("width : "+arrayz[i2].offsetWidth);
-                //document.getElementById('rewards_cont_scroll').scrollLeft-=arrayz[i2].offsetWidth;
-                arrayz[i2].style.display = "none";
-                modified = true;
-            }
-        }
-    }
-
-    if (modified) {
-        let divToModify = $('.pov-progress-bar-section');
-        if (divToModify.length > 0) {
-            $('.pov-progress-bar-section')[0].scrollTop = '0';
-        }
-    }
-}
 
 
-function moduleSimSeasonMaskReward() {
-    var arrayz;
-    var nbReward;
-    let modified = false;
-    arrayz = $('.rewards_pair:not([style*="display:none"]):not([style*="display: none"])');
-    if ($("div#gsp_btn_holder[style='display: block;']").length) {
-        nbReward = 1;
-    }
-    else {
-        nbReward = 2;
-    }
-
-    var obj;
-    //console.log("scroll before : "+document.getElementById('rewards_cont_scroll').scrollLeft);
-
-
-    if (arrayz.length > 0) {
-        for (var i2 = arrayz.length - 1; i2 >= 0; i2--) {
-            obj = $(arrayz[i2]).find('.tick_s:not([style*="display:none"]):not([style*="display: none"])');
-            if (obj.length >= nbReward) {
-                //console.log("width : "+arrayz[i2].offsetWidth);
-                //document.getElementById('rewards_cont_scroll').scrollLeft-=arrayz[i2].offsetWidth;
-                arrayz[i2].style.display = "none";
-                modified = true;
-            }
-        }
-    }
-    if (modified) {
-        let divToModify = $('#seasons_row1');
-        if (divToModify.length > 0) {
-            divToModify[0].style.transform = "translate3d(0px, 0px, 0px)";
-        }
-        divToModify = $('#ascrail2000-hr .nicescroll-cursors');
-        if (divToModify.length > 0) {
-            divToModify[0].style.left = '0px';
-        }
-    }
-    //console.log("scroll after : "+document.getElementById('rewards_cont_scroll').scrollLeft);
-}
 
 function moduleChangeTeam() {
     if (document.getElementById("ChangeTeamButton") !== null || document.getElementById("ChangeTeamButton2") !== null) {
